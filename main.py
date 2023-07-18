2ce852d42fca2afda4c8e13ef1bb66e1710d433a
breannablackwood <blackwoo.breanna@ufl.edu>
2023-07-18 12:04:17 -0400

    This is my latest edit of the main.py file.

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
-# Breanna Blackwood, 15 July 2023
-
+# Breanna Blackwood
 
 def encode(password):
 """ Encodes the user's inputted password by incrementing each number by 3."""
 	encoded_password = []  # create empty list
 	for num in password:  # increment each number in password by 3 for encoding
-		if num == 0:
-			num = 3
-		elif num == 1:
-			num = 4
-		elif num == 2:
-			num = 5
-		elif num == 3:
-			num = 6
-		elif num == 4:
-			num = 7
-		elif num == 5:
-			num = 8
-		elif num == 6:
-			num = 9
-		elif num == 7:
-			num = 0
-		elif num == 8:
-			num = 1
-		elif num == 9:
-			num = 2
-		encoded_password.append()  # add the new number to the list
-	return encoded_password
+		new = int(num) + 3
+		encoded_password.append(str(new))  # add the new number to the list
+	enc = "".join(encoded_password)
+	return enc
 
 
 def main():
